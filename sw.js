0d729294896fa822574aef240a6f390bc321b340
Planoafeto <nailton1alv@gmail.com>
2026-03-02 17:48:02 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,17 @@
+const CACHE_NAME = "planoafeto-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
 self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
   self.skipWaiting();
 });
 
@@ -7,5 +20,8 @@ self.addEventListener("activate", event => {
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
